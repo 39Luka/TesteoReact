89e4348ffd9c81a979abe7f9ae18846246f349b7
marinanavarropina <m.navarro4@edu.gva.es>
2025-09-19 05:39:34 +0200

    Ejemplo 2: Variables-- declaración, uso y hoisting

---
+++ UT02_02_Sintaxis_Funciones_Ejemplos/ejemplo2/ejemplo2.js
@@ -0,0 +1,35 @@
+//'use strict';//hace obligatorio declarar las variables
+//hola="hola";
+//var edad=15;//global hasta que encuentra otra global con el mismo identificador
+let edad=15;//bloque para el que se declara
+if (edad < 18) {
+    let textoLet = 'Eres menor de edad y te ponemos un año más';
+    var textoVar = 'Eres menor de edad y te ponemos un año más';
+
+} else {
+    let textoLet = 'Eres mayor de edad y te ponemos un año más';
+    var textoVar = 'Eres mayor de edad y te ponemos un año más';
+}
+edad = edad + 1;
+
+function verEdad(){
+    //edad=16;//global externa a la función
+    var edad=18;// global a esta función
+    if (edad < 18) {
+        let textoLet = 'Eres menor de edad y te ponemos un año más';
+        var textoVar = 'Eres menor de edad y te ponemos un año más';
+
+    } else {
+        let textoLet = 'Eres mayor de edad y te ponemos un año más';
+        var textoVar = 'Eres mayor de edad y te ponemos un año más';
+        
+    }
+    edad = edad + 1;
+    //console.log(textoLet);
+    console.log(textoVar);  // mostrará la cadena
+    console.log(edad);
+}
+//console.log(textoLet);  // mostrará undefined porque fuera del if no existe la variable -- ERROR!
+console.log(textoVar);  // mostrará la cadena
+console.log(edad);
+verEdad();
