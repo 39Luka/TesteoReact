d18575bf4a9002368d12ff8743d8f58dcc7d82b4
Marina Navarro Pina <153812395+marinanavarropina@users.noreply.github.com>
2025-12-10 07:51:21 +0100

    Delete UT04_Formularios_Ejemplos directory

--- UT04_Formularios_Ejemplos/src/pages/Admin.jsx
+++
@@ -1,9 +0,0 @@
-function Admin() {
-  return (
-    <>
-      <h2 className="text-xl font-semibold mb-4">Panel de Administración</h2>
-      <p>Aquí irían las herramientas de administración.</p>
-    </>
-  );
-}
-export default Admin;
--- UT04_Formularios_Ejemplos/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <>
-      <h2 className="text-xl font-semibold mb-4">Bienvenido</h2>
-      <p>Esta es la página principal de los ejemplos de formularios.</p>
-    </>
-  );
-}
-export default Home;
